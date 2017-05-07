510b6a4149809d2ff419cc2e663d0ff04bd8d8e1
Robert Kasperczyk <robertkasperczyk@gmail.com>
2017-05-07 23:19:24 +0200

    added conditional when user is logged in

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+    res.render('index', {user: {authenticated: req.session.authenticated}});
 });
 
 module.exports = router;
