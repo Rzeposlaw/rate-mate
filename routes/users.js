9d3055b363ae6e36769f400d624708aa59a21e16
Katarzyna Rzepecka <katarzyn.rzepecka@gmail.com>
2017-05-08 23:55:14 +0200

    Added wirting users table when admin logged

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
+var mongoose = db.mongoose;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var User = mongoose.model('User');
+
+router.get('/', function (req, res, next) {
+    User.find({}, function (err, users) {
+        res.render('users', {users: users});
+    });
 });
 
 module.exports = router;
