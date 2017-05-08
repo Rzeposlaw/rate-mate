09dd9cb2b53163ef29109afe54f8296a5d0a0a0e
Robert Kasperczyk <robertkasperczyk@gmail.com>
2017-05-08 18:19:29 +0200

    implemented file upload for new product view

--- routes/new-product.js
+++ routes/new-product.js
@@ -1,19 +1,28 @@
-/**
- * Created by robertt240 on 5/8/17.
- */
-/**
- * Created by robertt240 on 5/8/17.
- */
 var express = require('express');
 var router = express.Router();
 var session = require('express-session');
 var mongoose = require('mongoose');
+var fs = require('fs');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 /* GET home page. */
 var Product = mongoose.model('Product');
 
+
 router.get('/', function (req, res, next) {
     res.render('new-product');
+});
+
+router.post('/', multipartMiddleware, function (req, res, next) {
+    fs.readFile(req.files.productImage.path, function (err, data) {
+        var newPath = __dirname + "/../public/uploads/" + req.body.name + ".png";
+        console.log(newPath);
+        fs.writeFile(newPath, data, function (err) {
+            console.log(err);
+            res.redirect("/");
+        });
+    });
 });
 
 module.exports = router;
