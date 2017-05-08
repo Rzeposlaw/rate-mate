53d6e7e7bc32e0cc98cd0abe8505847fb3e0ca64
Robert Kasperczyk <robertkasperczyk@gmail.com>
2017-05-08 16:18:58 +0200

    sends list of products to product view and removed app.js from layout.pug

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,15 @@
 var express = require('express');
 var router = express.Router();
 var session = require('express-session');
-
+var mongoose = require('mongoose');
 
 /* GET home page. */
+var Product = mongoose.model('Product');
+
 router.get('/', function (req, res, next) {
-    res.render('products');
+    Product.find({}, function (err, products) {
+        res.render('products', {products: products});
+    });
 });
 
 module.exports = router;
