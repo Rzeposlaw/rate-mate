4b3d3fecb3a2b88c618261e1daf86a9bcc7975e3
Robert Kasperczyk <robertkasperczyk@gmail.com>
2017-05-11 10:57:13 +0200

    sending top products to index view

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,18 @@ var express = require('express');
 var router = express.Router();
 var session = require('express-session');
 
+var mongoose = require('mongoose');
+var Product = mongoose.model('Product');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index');
+
+    Product.find({}).sort({rating: -1}).limit(5).exec(
+        function (err, products) {
+            res.render('index', {topProducts: products});
+
+        }
+    );
 });
 
 module.exports = router;
